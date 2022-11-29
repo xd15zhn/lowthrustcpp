0152c3219723115d919396477693304854ac8ec1
xd15zhn <yz37zhn@163.com>
2022-11-29 11:19:02 +0800

    11.29 1107 laptop

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# 小推力地火转移轨道设计与优化
+# 小推力地火转移轨道设计与优化(3D轨道仿真)
 
 # 依赖库
-- <https://gitee.com/xd15zhn/simucpp>
-- <https://gitee.com/xd15zhn/raylib>
+- 微分方程求解器 <https://gitee.com/xd15zhn/simucpp>
+- 3D图形库 <https://gitee.com/xd15zhn/raylib>
+
+# 程序说明
+- [main.cpp] 仿真主程序，对应报告的4.3节。
+- [camera.c/h] 3D图形库的摄像头设置。
+- [elements.cpp/hpp] 开普勒轨道六要素，对应报告的1.2节。
+- [detector.cpp/hpp] 建立火星探测器被控对象模型，对应报告的4.2节。
+- [parameters.hpp] 需要用到的常数参数，对应报告的0，1.1，2，4.1节。
--- main.cpp
+++ main.cpp
@@ -8,9 +8,11 @@
 // #define DEBUG
 #define FULL_SCREEN
 
+// 类型转换
 Vector3 MatToVector3(Mat m) {
     return (Vector3) {float(m.at(0,0)), float(m.at(1,0)), float(m.at(2,0))};
 }
+// 画轨迹
 void Draw_Trajectory(list<Vector3>& point, Color color) {
     std::list<Vector3>::iterator it = point.begin();
     Vector3 posstart, posend;
@@ -23,44 +25,27 @@ void Draw_Trajectory(list<Vector3>& point, Color color) {
     }
 }
 
-class Cubic: public simucpp::UserFunc
-{
-public:
-    // Cubic(double a, double b, double c, double d):
-    //     _a(a), _b(b),_c(c), _d(d) {}
-    void Set(double a, double b, double c, double d)
-        {_a=a; _b=b;_c=c; _d=d;}
-    virtual double Function(double t) const {
-        double ans = KT*_a*t + _b;
-        ans = KT*ans*t + _c;
-        ans = KT*ans*t + _d;
-        return ans;
-    };
-private:
-    double _a, _b, _c, _d;
-};
-
 int main(void) {
 /**********************
 计算地球和火星轨道
 **********************/
-    Mat startR(3, 1), startV(3, 1);
-    Mat endR(3, 1), endV(3, 1);
-    list<Vector3> earthOrbit, marsOrbit;
-    list<Vector3>  usvOrbit1, usvOrbit2, usvOrbit3;
-    Orbital_Elements earthElements(SUN_MU, Mat(6, 1, vecdble{
+    Mat startR(3, 1), startV(3, 1);  // 出发点位置和速度向量
+    Mat endR(3, 1), endV(3, 1);  // 到达点位置和速度向量
+    list<Vector3> earthOrbit, marsOrbit;  // 存储地球和火星轨道轨迹点
+    list<Vector3>  usvOrbit1, usvOrbit2, usvOrbit3;  // 存储探测器的三段轨道轨迹点
+    Orbital_Elements earthElements(SUN_MU, Mat(6, 1, vecdble{  // 根据开普勒轨道六要素计算地球轨道
         EARTH_a, EARTH_e, EARTH_i, EARTH_n, EARTH_w, EARTH_f}));
-    Orbital_Elements marsElements(SUN_MU, Mat(6, 1, vecdble{
+    Orbital_Elements marsElements(SUN_MU, Mat(6, 1, vecdble{  // 根据开普勒轨道六要素计算火星轨道
         MARS_a, MARS_e, MARS_i, MARS_n, MARS_w, MARS_f}));
-    int t;
-    Mat point;
+    int t;  // 全局时间，单位千秒
+    Mat point;  // 轨迹点暂存
     for (t = 0; t < 1577; t++) {  // 365*86400/1e4/2
-        earthElements.Update_TrueAnomaly(t);
-        point = earthElements.Update_Position();
+        earthElements.Update_TrueAnomaly(t);  // 根据当前时间计算真近点角
+        point = earthElements.Update_Position();  // 根据当前真近点角计算位置向量
         earthOrbit.push_back(MatToVector3(point));  // 存储轨迹点
         if (t==0) {
-            startR = point;
-            startV = earthElements.Update_Velocity();
+            startR = point;  // 记录从地球出发的位置向量
+            startV = earthElements.Update_Velocity();  // 根据当前真近点角计算从地球出发的速度向量
         }
     }
     for (t = 0; t < 2968; t++) {  // 687*86400/1e4/2
@@ -68,8 +53,8 @@ int main(void) {
         point = marsElements.Update_Position();
         marsOrbit.push_back(MatToVector3(point));
         if (t==0) {
-            endR = point;
-            endV = earthElements.Update_Velocity();
+            endR = point;  // 记录到达火星的位置
+            endV = earthElements.Update_Velocity();  // 根据当前真近点角计算到达火星的速度向量
         }
     }
     cout << "start position, start velocity, end position, end velocity:" << endl;
@@ -81,35 +66,55 @@ int main(void) {
 /**********************
 计算转移轨道
 **********************/
-    vecdble solution{
+    vecdble solution{  // 最优解
         0.327971, -22.18204, -0.023281, 0.888952, -1.17108, -23.42276,
         0.760948, 14.537111, 0.256603, -8.726293, 0.510507, -1.214071,
         -1.76277, 8.449013, 0.773741, -5.83875, 0.94229, -0.752097,
     };
-    Cubic *cubic1 = new Cubic;
-    Cubic *cubic2 = new Cubic;
-    Orbital_Solver solver;
-    usvOrbit1.push_back(MatToVector3(startR));
-    solver._mssr->Set_InitialValue(Mat(vecdble{
+    // 以下为4个三次多项式
+    function<double(double)> cubicFunc1 = [solution](double t){
+        double ans = KT*solution[0]*t + solution[4];
+        ans = KT*ans*t + solution[8];
+        ans = KT*ans*t + solution[12];
+        return ans;
+    };
+    function<double(double)> cubicFunc2 = [solution](double t){
+        double ans = KT*1e-5*solution[1]*t + 1e-5*solution[5];
+        ans = KT*ans*t + 0.01*solution[9];
+        ans = KT*ans*t + 0.01*solution[13];
+        return ans;
+    };
+    function<double(double)> cubicFunc3 = [solution](double t){
+        double ans = KT*solution[2]*t + solution[6];
+        ans = KT*ans*t + solution[10];
+        ans = KT*ans*t + solution[14];
+        return ans;
+    };
+    function<double(double)> cubicFunc4 = [solution](double t){
+        double ans = KT*1e-5*solution[3]*t + 1e-5*solution[7];
+        ans = KT*ans*t + 0.01*solution[11];
+        ans = KT*ans*t + 0.01*solution[15];
+        return ans;
+    };
+    Orbital_Solver solver;  // 微分方程求解器
+    solver._mssr->Set_InitialValue(Mat(vecdble{  // 设置位置向量积分器的初值
         -113.101881186742, 101.033358324501, 0.00219405859350417}));
-    solver._mssv->Set_InitialValue(Mat(vecdble{
+    solver._mssv->Set_InitialValue(Mat(vecdble{  // 设置速度向量积分器的初值
         -19.3527970698371, -22.1182257893965, -0.000451706663468062}));
-    solver._intM->Set_InitialValue(USV_M);
+    solver._intM->Set_InitialValue(USV_M);  // 设置初始质量
     cout << "Calculating trajectory......" << endl;
     /*分别设置第一段和第三段的发动机工作时间*/
     int dur1 = 25000/PI * atan(solution[16]*solution[16]);
     int dur2 = FLY_TIME - 15000/PI * atan(solution[17]*solution[17]);
     /*第一段发动机工作，为俯仰角和方位角分别设置三次函数*/
-    cubic1->Set(solution[0], solution[4], solution[8], solution[12]);
-    cubic2->Set(1e-5*solution[1], 1e-5*solution[5], 0.01*solution[9], 0.01*solution[13]);
-    solver._inTheta->Set_Function(cubic1);
-    solver._inPhi->Set_Function(cubic2);
+    solver._inTheta->Set_Function(cubicFunc1);
+    solver._inPhi->Set_Function(cubicFunc2);
     solver._cnstF->Set_OutValue(USV_F);
     for (t = 0; t < dur1; t++) {
         // for (int i = 0; i < 100; i++)
             solver._sim1.Simulate_OneStep();
         point = solver._mssr->Get_OutValue();
-        usvOrbit1.push_back(MatToVector3(point));
+        usvOrbit1.push_back(MatToVector3(point));  // 存储轨迹点
     }
     /*第二段发动机关闭*/
     solver._cnstF->Set_OutValue(0);
@@ -120,10 +125,8 @@ int main(void) {
         usvOrbit2.push_back(MatToVector3(point));
     }
     /*第三段发动机工作，为俯仰角和方位角分别设置新的三次函数*/
-    cubic1->Set(solution[2], solution[6], solution[10], solution[14]);
-    cubic2->Set(1e-5*solution[3], 1e-5*solution[7], 0.01*solution[11], 0.01*solution[15]);
-    solver._inTheta->Set_Function(cubic1);
-    solver._inPhi->Set_Function(cubic2);
+    solver._inTheta->Set_Function(cubicFunc3);
+    solver._inPhi->Set_Function(cubicFunc4);
     solver._cnstF->Set_OutValue(USV_F);
     for (; t < FLY_TIME; t++) {
         // for (int i = 0; i < 100; i++)
@@ -162,11 +165,11 @@ int main(void) {
                 // DrawGrid(120, 5);
                 DrawSphere((Vector3){0.0f, 0.0f, 0.0f}, 10, ORANGE);  // 画3D球
                 // Draw_Frame_Oxyz();  // 坐标系测试
-                Draw_Trajectory(earthOrbit, RAYWHITE);  // 画3D轨迹
-                Draw_Trajectory(marsOrbit, RAYWHITE);  // 画3D轨迹
-                Draw_Trajectory(usvOrbit1, RED);  // 画3D轨迹
-                Draw_Trajectory(usvOrbit2, GREEN);  // 画3D轨迹
-                Draw_Trajectory(usvOrbit3, RED);  // 画3D轨迹
+                Draw_Trajectory(earthOrbit, RAYWHITE);  // 画地球轨道
+                Draw_Trajectory(marsOrbit, RAYWHITE);  // 画火星轨道
+                Draw_Trajectory(usvOrbit1, RED);  // 画第一段加速度轨道
+                Draw_Trajectory(usvOrbit2, GREEN);  // 画第二段无动力滑行轨道
+                Draw_Trajectory(usvOrbit3, RED);  // 画第三段加速度轨道
             EndMode3D();
             DrawText(TextFormat("%2i FPS", GetFPS()), 0, 0, 20, LIME);
         EndDrawing();
